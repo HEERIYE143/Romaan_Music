9958608c4c5ea533b597b2057f71ac1cabe144b0
HEERIYE143 <162871541+HEERIYE143@users.noreply.github.com>
2024-08-02 08:08:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://telegra.ph/file/07ae8401651748e06b9ef.mp4",
+    "http://t.me/PICK_UP_LINES_PROMOTION_BOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
