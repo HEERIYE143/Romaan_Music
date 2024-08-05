40c84e923b03c16cf980ac57a63b35240f167a2b
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-08-05 18:20:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/L2LUCKY/LuckyXMusic2",
+    "http://t.me/PICK_UP_LINES_PROMOTION_BOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
